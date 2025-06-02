06dbae4d4c2438a4e5d539e317ff741253b4fca8
Nursultan <nmakhamatov@ya.ru>
2025-06-02 23:12:58 +0600

    done

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:meals/data/dummy_data.dart';
 import 'package:meals/models/category.dart';
+import 'package:meals/models/meal.dart';
 import 'package:meals/screens/meals.dart';
 import 'package:meals/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
-  const CategoriesScreen({super.key});
+  const CategoriesScreen({
+    super.key,
+    required this.onToggleFavorite,
+    required this.availableMeals,
+    });
+
+  final void Function(Meal meal) onToggleFavorite;
+  final List<Meal> availableMeals;
 
   void _selectCategory(BuildContext context, Category category) {
-    final filteredMeals = dummyMeals
+    final filteredMeals = availableMeals
         .where((meal) => meal.categories.contains(category.id))
         .toList();
 
@@ -16,15 +24,14 @@ class CategoriesScreen extends StatelessWidget {
         builder: (ctx) => MealsScreen(
             title: category.title,
             meals: filteredMeals,
+            onToggleFavorite: onToggleFavorite,
         )
     ));
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Pick your category')),
-      body: GridView( // для динамического добавления данных нужно исп-ть GridView.builder
+    return GridView( // для динамического добавления данных нужно исп-ть GridView.builder
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
           childAspectRatio: 3 / 2,
@@ -41,7 +48,6 @@ class CategoriesScreen extends StatelessWidget {
             )
         ],
         padding: EdgeInsets.all(16),
-      ),
-    );
+      );
   }
 }
